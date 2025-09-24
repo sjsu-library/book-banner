6a42163c1295785740d281dec6fb142a5090a18f
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2025-09-24 14:27:40 -0700

    Add Google Books python library

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,56 +20,8 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 import os
 import streamlit as st
 from pyalex import Works
+from google_books_api_wrapper.api import GoogleBooksAPI
 
-# @st.cache_resource(ttl="1d", show_spinner=False)
-# def createVectorStore():
-#    loader = CSVLoader(file_path="data.csv")
-#    data = loader.load()
-#    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-#    vectorstore = Chroma.from_documents(
-#                        data,                 # Data
-#                        embeddings    # Embedding model
-#                        )
-#    retriever = vectorstore.as_retriever()
-#    return retriever
-
-
-# addition tool
-@tool    # This identifies the following function as a tool to langgraph
-# in the following statement the function name, the attributes their types and the output type are defined
-def addition(x:int, y:int) -> int : 
-    # The following docstring describes what the function can do and is used by the LLM to determine whethere this
-    # is the tool to be called, and what are its inputs and outputs
-    """
-    This addition function adds two numbers and returns their sum. 
-    It takes two integers as its inputs and the output is an integer.
-    """
-    return x + y
-
-# subtraction tool
-@tool
-def subtraction(x:int, y:int) -> int : 
-    """
-    This subtraction function subtracts a number from another and returns the difference. 
-    It takes two integers as its inputs and the output is an integer.
-    """
-    return x - y
-# multiplication tool
-@tool
-def multiplication(x:int, y:int) -> int : 
-    """
-    This multiplication function multiplies two numbers returns the product. 
-    It takes two integers as its inputs and the output is an integer.
-    """
-    return x * y
-# division tool
-@tool
-def division(x:int, y:int) -> int : 
-    """
-    This division function divides one number by another and returns the quotient. 
-    It takes two integers as its inputs and the output is an integer.
-    """
-    return x / y
 
 @tool
 def openAlex(term: str) -> str :
@@ -83,15 +35,19 @@ def openAlex(term: str) -> str :
         response = "Could not extract search term"
     return response
 
-#@tool
-#def localDocs(query:str) -> str :
-#    """
-#    Searches for inforamtion about SJSU library and returns relevant documents
-#    """
-#    docs = retriever.invoke(query)
-#    return docs
+@tool
+def googleBooks(term:str) -> str :
+    """
+    Searches for books in the Google Books API using a keyword.
+    """
+    response = []
+    if term
+        books = client.get_books_by_subject(term)
+        response = books.get_all_results()[:10]
+    else:
+        response = "Could not extract search term"
+    return response
 
-#createVectorStore()
 
 st.title("Arithmetic Agent Chatbot")
 
@@ -104,7 +60,7 @@ os.environ["GOOGLE_API_KEY"] = st.secrets.key
 
 # LLM configuration
 model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.0-flash-lite",
     temperature=0,
     max_tokens=None,
     timeout=None,
@@ -135,21 +91,20 @@ for message in st.session_state.messages:
 #    print(response.content)
 
 # Tool list
-arithmeticagent_tools = [addition, subtraction, multiplication, division, openAlex]
+bookbanagent_tools = [openAlex, googleBooks]
 
-arithmeticagent_system_prompt = SystemMessage(
-    """You are a math agent that can solve simple mathematics problems like addition, subtraction, multiplication and division. 
-    Solve the mathematics problems provided by the user using only the available tools and not by yourself. Provide the answer given by the tool.  
-    You are also able to take a natural language query and fetch and return a list of five relevant research articles with links from the OpenAlex API.
+bookbanagent_system_prompt = SystemMessage(
+    """You are a book banning agent that can identify book and article titles that should be removed from circulation because they violate content restrictions.
+    Identify titles to be banned using only the available tools and not by yourself. Provide the answer given by the tool.
     """
 )
 
 debug_yn = True
 debug_yn = st.checkbox("Debug")
-arithmeticagent_graph = create_react_agent(
+bookbanagent_graph = create_react_agent(
     model = model,
-    state_modifier = arithmeticagent_system_prompt,
-    tools = arithmeticagent_tools,
+    state_modifier = bookbanagent_system_prompt,
+    tools = bookbanagent_tools,
     debug = debug_yn)
 
 
@@ -164,7 +119,7 @@ if prompt := st.chat_input("Enter a simple mathematics question"): # if input ha
 
     with st.chat_message("assistant"):    
         inputs = {"messages":[("user", prompt)]}
-        result = arithmeticagent_graph.invoke(inputs)
+        result = bookbanagent_graph.invoke(inputs)
         response = f"Agent response : {result['messages'][-1].content} \n"
         if detailed_yn:
             response = response + f"\nDetailed execution flow : \n"
